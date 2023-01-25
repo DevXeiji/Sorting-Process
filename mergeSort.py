a253e8481a75b73271ad8218e1b04cb7894ddb8b
DevPapel <cool.jef77@gmail.com>
2023-01-25 19:05:19 +0800

    mergeSort() function updated

--- mergeSort.py
+++ mergeSort.py
@@ -1,1 +1,8 @@
-def mergeSort():
+def mergeSort(num):
+    if len(num) > 1:
+        temp = len(num)//2
+        LFT = num[:temp]
+        RGT = num[temp:]
+
+        mergeSort(LFT)
+        mergeSort(RGT)
