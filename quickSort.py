b0a847ad1929eb20bc96962560fcd33932b5d7b4
DevPapel <cool.jef77@gmail.com>
2023-01-25 20:09:34 +0800

    partition function updated

--- quickSort.py
+++ quickSort.py
@@ -1,6 +1,12 @@
 def partition(num, low, high):
     pivot = num[high]
-    print(pivot)
+
+    x = low - 1
+    for y in range(low, high):
+        if num[y] <= pivot:
+            x = x + 1
+            (num[x], num[y]) = (num[y], num[x])
+    (num[x + 1], num[high]) = (num[high], num[x + 1])
 
 num = [7, 26, 86, 88, 73, 59, 19, 18, 44, 46]
 print(num)
