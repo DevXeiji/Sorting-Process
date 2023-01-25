bdd92969bda43c61e601fbfbf5f0558ccf7fa7b4
DevPapel <cool.jef77@gmail.com>
2023-01-25 15:03:19 +0800

    insertionSort() function updated

--- insertionSort.py
+++ insertionSort.py
@@ -1,5 +1,10 @@
 def insertionSort(num):
     for x in range(1, len(num)):
+        sort = num[x]
+        while num[x-1] > sort and x>0:
+            num[x], num[x-1] = num[x-1], num[x]
+            x = x - 1
 
 num = [7, 26, 86, 88, 73, 59, 19, 18, 44, 46]
+insertionSort(num)
 print(num)
