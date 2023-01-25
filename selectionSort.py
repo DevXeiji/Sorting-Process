656638b9f2e72c6091f25c2983894fc999aa2139
DevPapel <cool.jef77@gmail.com>
2023-01-25 13:55:40 +0800

    Implementation of debugger using print

--- selectionSort.py
+++ selectionSort.py
@@ -2,5 +2,9 @@ def selectionSort():
     for x in range(10):
         pos = x
         for y in range(x,11):
-            if nums[y] < nums[pos]:
+            if num[y] < num[pos]:
                 pos = y
+
+num = [7, 26, 86, 88, 73, 59, 19, 18, 44, 46]
+selectionSort(num)
+print(num)
