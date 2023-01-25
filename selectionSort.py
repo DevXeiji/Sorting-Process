f07c3819cab593cdc1badef01bbe213915b1f8cc
DevPapel <cool.jef77@gmail.com>
2023-01-25 13:58:36 +0800

    y range adjusted to 9 to fix the index out of range error

--- selectionSort.py
+++ selectionSort.py
@@ -1,9 +1,12 @@
-def selectionSort():
+def selectionSort(num):
     for x in range(10):
         pos = x
-        for y in range(x,11):
+        for y in range(x,9):
             if num[y] < num[pos]:
                 pos = y
+
+        print(num)
+
 
 num = [7, 26, 86, 88, 73, 59, 19, 18, 44, 46]
 selectionSort(num)
