f59ed84ba123f441fe9cf550f4fcc72eb0d2e6b3
DevPapel <cool.jef77@gmail.com>
2023-01-25 14:36:26 +0800

    Updated bubbleSort() function

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,7 +1,11 @@
 def bubbleSort(num):
     for x in range(len(num)-1,0,-1):
-
-
+        for y in range(x):
+            if num[y]>num[y+1]:
+                temp = num[y]
+                num[y] = num[y+1]
+                num[y+1] = temp
 
 num = [7, 26, 86, 88, 73, 59, 19, 18, 44, 46]
+bubbleSort(num)
 print(num)
