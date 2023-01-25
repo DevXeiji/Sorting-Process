3ab32d139b71dc3bbf2ec1c5ae68e41ddabc8eab
DevPapel <cool.jef77@gmail.com>
2023-01-25 15:04:07 +0800

    Added debug print to insertionSort() function to show the sorting process

--- insertionSort.py
+++ insertionSort.py
@@ -4,7 +4,7 @@ def insertionSort(num):
         while num[x-1] > sort and x>0:
             num[x], num[x-1] = num[x-1], num[x]
             x = x - 1
+            print(num)
 
 num = [7, 26, 86, 88, 73, 59, 19, 18, 44, 46]
 insertionSort(num)
-print(num)
