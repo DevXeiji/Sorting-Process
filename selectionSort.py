f9c629c1232d0ade0d8dbf8bdb98859f3907a116
DevPapel <cool.jef77@gmail.com>
2023-01-25 14:09:31 +0800

    Added temp variable and adjusted x range in selectionSort()

--- selectionSort.py
+++ selectionSort.py
@@ -1,10 +1,13 @@
 def selectionSort(num):
-    for x in range(10):
+    for x in range(9):
         pos = x
-        for y in range(x,9):
+        for y in range(x,10):
             if num[y] < num[pos]:
                 pos = y
 
+        temp = num[x]
+        num[x] = num[pos]
+        num[pos] = temp
         print(num)
 
 
